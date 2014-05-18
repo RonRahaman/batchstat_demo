5cab10fc2b9d5f618e226f68f3b1df5035bbf91c
Ron Rahaman <rahaman@cs.uchicago.edu>
2014-05-18 16:24:30 -0500

    switched SourceBank to 2d

--- particle.py
+++ particle.py
@@ -2,8 +2,8 @@ import numpy as np
 
 phase_space_dtype = np.dtype([
     ('wgt', 'float32'),
-    ('pos', 'float32', 3),
-    ('dir', 'float32', 3),
+    ('pos', 'float32', 2),
+    ('dir', 'float32', 2),
     ('e',   'float32')
     ])
 
@@ -12,25 +12,25 @@ tally_dtype = np.dtype([
     ])
 
 class SourceBank(object):
+    """ A bank of sites from a point, isotropic, monoenergetic source.
 
+    Args:
+        n_sites (int): number of source sites
+        pos ([float, float]): position in 2D cartesian space
+        energy (float): energy of source
+
+    Attributes:
+        array (np.array(dtype=phase_space_dtype)): array of banked sites
+
+    """
     def __init__(self, n_sites, pos, energy):
-        self.sample_external_source(n_sites, pos, energy)
-        return
-
-    def sample_external_source(self, n_sites, pos, energy):
         self.array = np.empty(n_sites, dtype=phase_space_dtype)
         self.array['wgt'] = 1.
         self.array['pos'] = pos
-        self.array['dir'] = self._sample_isotropic_angles(n_sites)
+        self.array['dir'] = [[np.cos(phi), np.sin(phi)] 
+                for phi in 2.*np.pi*np.random.random(n_sites) ]
         self.array['e']   = energy
         return
-
-    def _sample_isotropic_angles(self, n):
-        phi = 2.*np.pi*np.random.random()
-        mu  = 2.*np.random.random() - 1.
-        return [[ mu, 
-                np.sqrt(1.-mu**2) * np.cos(phi),
-                np.sqrt(1.-mu**2) * np.sin(phi) ] for i in range(n) ]
 
 class TallyBins(object):
 
